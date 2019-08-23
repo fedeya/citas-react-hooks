8e72bea80c2d09bdbd07a97baac214106ee2a14b
Federico Minaya <elfedeminaya@gmail.com>
2019-08-23 11:31:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from 'react';
+import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
+
+  let initCitas = JSON.parse(localStorage.getItem("citas"));
+  if(!initCitas){
+    initCitas = [];
+  }
+
+  const [citas, setCitas] = useState(initCitas);
+
+  useEffect(() => {
+    const getCitas = JSON.parse(localStorage.getItem("citas"));
+    if(getCitas){
+      localStorage.setItem("citas", JSON.stringify(citas));
+    } else {
+      localStorage.setItem("citas", JSON.stringify([]));
+    }
+  }, [citas]);
+
+  const crearCita = cita => {
+    
+    const newCitas = [...citas, cita];
+
+    setCitas(newCitas);
+  }
+
+  const deleteCita = id => {
+    const newCitas = [...citas];
+    newCitas.splice(id, 1);
+    setCitas(newCitas);
+  }
+
+  const message = citas.length === 0 ? "No Hay Citas" : "Administra tu Citas";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario crearCita={crearCita}  />
+          </div>
+          <div className="one-half column">
+            <h2>{message}</h2>
+            {
+              citas.map((cita, index) => (
+                <Cita
+                  key={index}
+                  id={index}
+                  cita={cita}
+                  deleteCita={deleteCita}
+                />
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,54 @@
+import React, { useState, Fragment } from "react";
+
+function Formulario({ crearCita }) {
+
+  const [cita, setCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: ""
+  });
+
+  const handleChange = e => {
+    setCita({
+      ...cita,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  const sendCita = e => {
+    e.preventDefault();
+    crearCita(cita);
+    e.target.reset();
+  }
+
+  return (
+    <Fragment>
+      <h2>Crear Cita</h2>
+      <form onSubmit={sendCita} >
+        <label>Nombre Mascota</label>
+        <input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" onChange={handleChange} />
+
+        <label>Nombre Dueño</label>
+        <input type="text" name="propietario" className="u-full-width" placeholder="Nombre Dueño de la Mascota" onChange={handleChange} />
+
+        <label>Fecha</label>
+        <input type="date" name="fecha" className="u-full-width" onChange={handleChange} />
+
+
+        <label>Hora</label>
+        <input type="time" name="hora" className="u-full-width" onChange={handleChange} />
+
+
+        <label>Sintomas</label>
+        <textarea name="sintomas" className="u-full-width" onChange={handleChange} />
+
+        <button className="button-primary u-full-width" type="submit">Agregar</button>
+
+      </form>
+    </Fragment>
+  )
+}
+
+export default Formulario;
